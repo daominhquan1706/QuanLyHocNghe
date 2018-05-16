7231cdb552c6803597cb91b5e5988fcd7a27f864
daominhquan176 <daominhquan176@gmail.com>
2018-05-16 16:18:28 +0700

    test

---
+++ QuanLyHocNghe-visual studio 2018/WebsiteQuanLyHocNgheCuaHung/WebsiteQuanLyHocNgheCuaHung/Areas/SinhVien/Controllers/SinhVienController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteQuanLyHocNgheCuaHung.Areas.SinhVien.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        // GET: SinhVien/SinhVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
